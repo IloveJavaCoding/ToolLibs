f77b9a8fa9c667d3ccf011cfe7ce85a5572dd585
Zhangsl <547125836@qq.com>
2020-06-01 10:38:30 +0800

    123

---
+++ app/src/main/java/com/example/toollibs/Activity/UI/Activity_View_Calendar.java
@@ -0,0 +1,16 @@
+package com.example.toollibs.Activity.UI;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.toollibs.R;
+
+public class Activity_View_Calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__view__calendar);
+    }
+}
