4f90c4868c766db15d442bb0e553025e5581b2c7
Zhangsl <547125836@qq.com>
2020-06-09 16:45:11 +0800

    123

--- app/src/main/java/com/example/toollibs/Activity/Receiver/BootReceiver.java
+++ app/src/main/java/com/example/toollibs/Activity/Receiver/BootReceiver.java
@@ -3,6 +3,9 @@ package com.example.toollibs.Activity.Receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
 
 import com.example.toollibs.Activity.Config.Constant;
 import com.example.toollibs.Activity.Config.SettingData;
@@ -11,11 +14,23 @@ import com.example.toollibs.Activity.MainActivity;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) &&
-                SettingData.getBoolean(context, Constant.CONFIG_FILE, Constant.AUTO_START_KEY, Constant.AUTO_START_DEFAULT)){
-            startApp(context);
+        if(TextUtils.isEmpty(intent.getAction())) return;
+        if(SettingData.getBoolean(context, Constant.CONFIG_FILE, Constant.AUTO_START_KEY, Constant.AUTO_START_DEFAULT)){
+            //延迟启动activity
+            Message obMessage = handler.obtainMessage();
+            obMessage.obj = context;
+            handler.sendMessageDelayed(obMessage, 3000);
         }
     }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Context context = (Context) msg.obj;
+            context.startActivity(new Intent(context, MainActivity.class));
+            super.handleMessage(msg);
+        }
+    };
 
     private void startApp(Context context){
         Intent thisIntent = new Intent(context, MainActivity.class);
